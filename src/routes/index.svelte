668917598d9c0fad22d399ca96f0c88ea5ad4119
Vikram B Rojo <1778311+vikramrojo@users.noreply.github.com>
2021-05-09 20:40:59 -0600

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,36 +14,34 @@
   <div class="col">
     <h2>I'm Vikram, a product designer and systems thinker.</h2>
     <p>
-      Working from problem definition through to usability testing, I design
-      software that make developers more productive. I also implement design
-      systems and grow design operations. Currently, I work remotely for LaunchDarkly from
-      Colorado.
+      Working from problem definition through to usability testing, I design workflows for developers while growing design operations.
+      I joined LaunchDarkly in 2018 as the first product designer and in 2021 started at WorkOS.
     </p>
     <p>
       Before working in product, I was a high school technology teacher and Drupal
-      developer. And way before that I was just a kid prototyping apps in Visual
-      Basic and making flyers in Photoshop on an x86. I'm not actively looking for work
-      but I'd love to hear what you're working on. Email me, atavikram at gmail
+      developer. Well before that I was a kid prototyping apps in Visual
+      Basic and designing flyers in Photoshop on an x86. I'm not actively looking for work
+      but I'd love to hear what you're working on. Email me, vikramrojo at gmail
       dot com.
     </p>
     <h3>LaunchDarkly</h3>
     <p>
-      I currently work at LaunchDarkly as a design lead in their Core Application
+      I worked at LaunchDarkly as a design lead for their Core Application
       squad. Our team is tasked with improving understanding and
-      adoption of feature management, and how to effectively serve billions of flags daily to enterprise
-      and commercial customers. As part of that work, we're reworking our information architecture to
-      support better navigation, data displays and rollout configuration.
+      adoption of feature management. As part of that work, we've reviewed our information architecture to
+      support better navigation, table displays, filters and rollout configurations.
     </p>
     <p>
       Prior to this, I was responsible for building a design system across Figma
-      and Storybook during the course of feature development. Adding to this
+      and Storybook. Adding to this
       operational work, I've developed other standards such as designs
       briefs, interface inspections, whiteboarding and story mapping sessions.
     </p>
     <p>
-      My most recent project involved migrating our documentation site to a
+      My favorite project involved migrating our documentation site to a
       modern development stack. Central to this process was defining and
       prototyping a design system in React in parallel with Figma.
+      My more recent work was as a lead on relaunching our Experimentation offering.
     </p>
     <h3>Gigster</h3>
     <p>
