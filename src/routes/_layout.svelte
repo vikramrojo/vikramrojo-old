8f9991cdb06dcd482f8cadcd6905c5e7697b6999
Vikram B Rojo <1778311+vikramrojo@users.noreply.github.com>
2021-05-09 20:32:37 -0600

    Update _layout.svelte

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -18,10 +18,10 @@
   <footer class="wid100">
     <nav class="nav cols">
       <div class="navLeft col">
-        <a href="https://dribbble.com/atav1k">Github</a>
-        <a href="https://www.linkedin.com/in/atav1k/">LinkedIn</a>
-        <a href="https://medium.com/@atav1k/">Medium</a>
-        <a href="https://twitter.com/atav1k">Twitter</a>
+        <a href="https://github.com/vikramrojo">Github</a>
+        <a href="https://www.linkedin.com/in/vikramrojo/">LinkedIn</a>
+        <a href="https://medium.com/@vikramrojo/">Medium</a>
+        <a href="https://twitter.com/vikramrojo">Twitter</a>
         <a href="/">&copy; {new Date().getFullYear()}</a>
       </div>
     </nav>
